53116c07dbda55ff06022e29d5696702284e5b29
Anak553 <anak9658@gmail.com>
2019-11-21 22:22:18 +0200

    first assignment of javascript

---
+++ js/main.js
@@ -0,0 +1,308 @@
+
+
+
+
+// function addProduct(){
+//     var x = document.getElementById("productName").value ;
+//     var y = document.getElementById("productPrice").value ;
+//     var z = document.getElementById("productCatg").value ;
+   
+
+
+
+//     document.getElementById("demo").innerHTML= 
+// }
+
+
+
+
+// assignmet1
+
+// function numb(){
+//    var inter = document.getElementById("inter").value ;
+//     document.getElementById("print").innerHTML= inter ;
+// }
+
+
+// function numb2(){
+//     var inp =document.getElementById("inp").value;
+
+//     if(inp % 3 == 0){
+//         document.getElementById("print").innerHTML="yes" 
+//     }
+
+//     else if( inp % 4 == 0){
+//         document.getElementById("print").innerHTML="yes" 
+//     }
+
+//     else{
+//         document.getElementById("print").innerHTML="no" 
+//     }
+
+// }
+
+
+// var sl = [];
+// function addProduct(){
+//     var productName = document.getElementById("productName").value ;
+//     var productPrice = document.getElementById("productPrice").value ;
+//     var productCategery = document.getElementById("productCategery").value ;
+//     var productCode = document.getElementById("productCode").value ;
+
+    
+
+    // console.log("name:" + productName);
+    // console.log("price:" +  productPrice);
+    // console.log("Categery:" + productCategery );
+    // console.log("code:" + productCode );
+
+//     var dash =  productCode.search("-");
+//     var s =  productCode.slice(dash,dash+1)
+//     var np =   productCode.slice(0, dash);
+//     var pb =   productCode.slice(dash +1,  productCode.length);
+
+
+//    console.log(dash);
+//    console.log("company =" + np);
+//    console.log("brand =" + pb);
+//    console.log("dash =" + s);
+
+//     var prod ={
+//         name:productName,
+//         price:  productPrice,
+//         Categery: productCategery,
+//         code:productCode 
+//     }
+
+    
+//     sl.push(prod) ;
+//     console.log(sl);
+    
+
+// }
+
+
+// assignment3
+
+    // function prinN(){
+
+    //     var inputNumber1 = document.getElementById("inputNumber1").value;
+    
+    //     var inputNumber2 = document.getElementById("inputNumber2").value;
+    
+    //     if(inputNumber1 > inputNumber2 ){
+          
+    //         document.getElementById("larg").innerHTML="the largest number is :" +inputNumber1
+    //     }
+    //     else if(inputNumber2 > inputNumber1){
+    //         document.getElementById("larg").innerHTML="the largest number is :" +inputNumber2
+    //     }
+        
+    // }  
+
+ 
+
+
+    // asignment4
+
+    // function typeNumber(){
+    //     var typeN = document.getElementById("typeN").value ;
+    //     if(typeN >= 0){
+    //         document.getElementById("typeValue").innerHTML= "positiv";
+    //     }
+    //     else{
+    //         document.getElementById("typeValue").innerHTML= "negtive";
+    //     }
+    // }
+
+
+
+    //assignment 5
+
+    // function inputValues(){
+    //     var inputValue1 =document.getElementById("inputValue1").value ;
+    //     var inputValue2 =document.getElementById("inputValue2").value ;
+    //     var inputValue3 =document.getElementById("inputValue3").value ;
+
+
+    //     if( inputValue1 > inputValue2 && inputValue1 > inputValue3){
+    //         document.getElementById("outValue").innerHTML= "the gretest value is :" + inputValue1 ;
+    //     }
+    //     else if(inputValue2 > inputValue1 && inputValue2 > inputValue3){
+
+    //         document.getElementById("outValue").innerHTML="the gretest value is :" + inputValue2 ;
+    //     }
+    //     else if(inputValue3 > inputValue1 && inputValue3 > inputValue2){
+
+    //         document.getElementById("outValue").innerHTML="the gretest value is: " + inputValue3 ;
+    //     }
+
+
+    //    if(inputValue1 < inputValue2 && inputValue1 < inputValue3){
+
+    //         document.getElementById("outValue1").innerHTML="the smallest value is: " + inputValue1 ;
+
+    //     }
+    //     else if(inputValue2 < inputValue1 && inputValue2 < inputValue3){
+
+    //         document.getElementById("outValue1").innerHTML="the smallest value is: " + inputValue2 ;
+
+    //     }
+    //     else if(inputValue3 < inputValue1 && inputValue3 < inputValue2){
+
+    //         document.getElementById("outValue1").innerHTML="the smallest value is: " + inputValue3 ;
+
+    //     }
+
+    // }
+
+
+    //assignment6 
+
+    // function typeNum(){
+
+    //     var inpNumber = document.getElementById("inpNumber").value ;
+
+
+    //     if(inpNumber % 2 == 0){
+    //         document.getElementById("typeo").innerHTML= "the type numper is : even "
+    //     }
+    //     else{
+    //         document.getElementById("typeo").innerHTML= "the type numper is : odd "
+    //     }
+    // }
+
+
+    // assingment 7
+
+    // function typChar(){
+    //      var inpChar = document.getElementById("inpChar").value ;
+
+    //     if(inpChar == "a" || inpChar == "e" || inpChar == "i" || inpChar == "o" ||inpChar == "u"){
+    //         document.getElementById("char").innerHTML= "vowel" ;
+    //     }
+    //     else{
+    //         document.getElementById("char").innerHTML= "constant" ;
+    //     }
+    // }
+
+
+
+    // assignment8
+
+    // function inpav(){
+    //     var inpnum3 = document.getElementById("inpnum3").value ;
+    //     for(var i = 1 ;i <= inpnum3; i++ ){
+    //         // document.getElementById("num3").innerHTML= i + "," ;
+    //         document.write(i+",");
+    //     }
+    // }
+
+
+    // assignment 9
+
+    // function tabl(){
+    //     var inptNumb = document.getElementById("inptNumb").value ;
+
+    //     for(var i = 1 ; i <= 12 ; i++){
+    //         document.write(i*inptNumb +" ");
+    //     }
+    // }
+
+
+    // assignment 10
+
+
+    // function calcu(){
+    //     var inptNumb = document.getElementById("inptNumb").value ;
+
+    //     for(var i = 1 ; i <= inptNumb ; i++){
+    //         if(i % 2 == 0){
+    //             document.write(i + " ")
+    //         }
+    //     }
+    // }
+
+
+    // assignment 11
+
+// function calculatPOwer(){
+//     var inputNumbr1 = document.getElementById("inputNumbr1").value ;
+//     var inputNumbr2 = document.getElementById("inputNumbr2").value ;
+
+//     document.getElementById("Numbr").innerHTML=  inputNumbr1**inputNumbr2 ;
+// }
+
+
+// assignment 12 
+
+
+// var arr = [2,5,20,7,9,7,8] ;
+
+
+//     console.log(arr[0] +arr[])
+
+    // for(var i = 0 ; i < arr.length ; i++){
+    //     console.log(arr[i] + arr[i] );
+    // }
+
+
+    // assignment 13 
+
+
+   // assignment 15
+
+//    function av(x,y,z){
+
+//     console.log((x+y+z) /3)
+//    }
+
+//    av(1,2,3)
+
+
+
+  
+// assignment 16
+
+//  function intp(int){
+//      if(int%2 > 0){
+//          console.log("true");
+//      }
+//      else{
+//         console.log("false");
+//      }
+//  }
+ 
+//  intp(11);
+
+
+// assignment 17
+
+// function hambo([x,y,z]){
+//     console.log(x+y+z)
+// }
+
+// hambo([1,2,3]);
+
+
+   // assignment 18
+
+
+//    function chars(cha){
+//        if(cha == "a" || cha=="e"|| cha== "i" || cha== "o" || cha=="u"){
+//            console.log("true")
+//        }
+//        else{
+//         console.log("false")
+//        }
+//    }
+//    chars("b");
+
+ // assignment 19
+
+
+    
+
+   
+
+    
